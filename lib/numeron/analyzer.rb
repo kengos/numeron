89c77d72f2cf0752c0f47eea0426726cf09e5674
kengos <kengo@kengos.jp>
2012-11-18 14:50:05 +0900

    解が確定するケースをどうするか。。。

--- lib/numeron/analyzer.rb
+++ lib/numeron/analyzer.rb
@@ -7,61 +7,111 @@ module Numeron
       @calculator = calculator
     end
 
-    def run(depth = 1, params = {})
+    # 次の1手の計算
+    # @param [Symbol] mode :average or :worst
+    # @return [Hash] {recommend: [Array], size: Number}
+    def run(mode = :average, options = {})
+      case mode
+      when :average
+        run_average_mode(options[:cases])
+      when :worst
+        run_worstcase_mode(options[:worst_case])
+      when :possibilities
+        run_possibilities
+      end
+    end
+
+    # 最悪のケースを0e1b, 1e0bと想定し、それぞれの結果が出た際に可能性の平均値が最も少なくなるケースを推定する
+    def run_average_mode(cases = nil)
+      cases ||= [
+        {eat: 0, bite: 1},
+        {eat: 1, bite: 0}
+      ]
       recommend = []
       min_size = 10000
-      all_list.each_with_index do |f, i|
-        ex = calc_expected_value(f)
-        if min_size == ex
+      all_list.each do |f|
+        average = calc_average(f, cases)
+        if min_size == average
           recommend << f
-          min_size = ex
-        elsif min_size > ex
+        elsif min_size > average
           recommend = [f]
-          min_size = ex
+          min_size = average
         end
-        # _calculator = build_calculator
-        # _calculator.input(f, 0, 1)
-        # if i == 0
-        #   recommend = [f]
-        #   min_size = _calculator.possibilities.size
-        # else
-        #   if min_size == _calculator.possibilities.size
-        #     recommend << f
-        #   elsif _calculator.possibilities.size > 0 && min_size > _calculator.possibilities.size
-        #     recommend = [f]
-        #     min_size = _calculator.possibilities.size
-        #   end
-        # end
       end
 
       { recommend: recommend, size: min_size }
     end
 
-    # attackの結果、推定される結果に応じた可能性の数に応じた期待値を計算する
-    def calc_expected_value(attack)
-      result = []
-      [
+    # 最悪のケースを0e1bと推定し、その結果として最も可能性の数が最も少なくなるケースを推定する
+    def run_worstcase_mode(worst_case = {})
+      worst_case = {eat: 0, bite: 1}.merge(worst_case)
+      min_size = 10000
+      recommend = []
+      all_list.each do |f|
+        _calculator = build_calculator
+        _calculator.input(f, worst_case[:eat], worst_case[:bite])
+        if min_size == _calculator.possibilities.size
+          recommend << f
+        elsif min_size > _calculator.possibilities.size
+          recommend = [f]
+          min_size = _calculator.possibilities.size
+        end
+      end
+
+      { recommend: recommend, size: min_size }
+    end
+
+    def run_possibilities
+      possibilities = @calculator.possibilities.clone
+
+      recommend = []
+      min_size = 10000
+      cases = [
         {eat: 0, bite: 0},
         {eat: 0, bite: 1},
         {eat: 0, bite: 2},
-        {eat: 0, bite: 3},
         {eat: 1, bite: 0},
-        {eat: 1, bite: 2},
+        {eat: 1, bite: 1},
         {eat: 2, bite: 0}
-      ].each do |pattern|
+      ]
+      possibilities.each do |f|
+        average = calc_average(f, cases, true)
+        if min_size == average
+          recommend << f
+        elsif min_size > average
+          recommend = [f]
+          min_size = average
+        end
+      end
+
+      { recommend: recommend, size: min_size }
+    end
+
+    # attackの結果、推定される結果に対する可能性の数の平均を計算する
+    # @example
+    #   [10, 20, 10, 0, 5, 0, 5]の場合は ( 10 + 20 + 10 + 5 + 5 ) / 5 = 10
+    def calc_average(attack, cases, can_zero = false)
+      result = []
+      cases.each do |pattern|
         _calculator = build_calculator
         _calculator.input(attack, pattern[:eat], pattern[:bite])
         result << _calculator.possibilities.size
       end
 
-      result.tapp
       n = 0
       sum = 0
       result.each do |f|
         sum = sum + f
         n = n + 1 if f > 0
       end
-      sum / n
+
+      if n > 0
+        return sum.to_f / n.to_f
+      elsif can_zero
+        return 0
+      else
+        return 10000
+      end
     end
 
     def build_calculator
--- spec/lib/numeron/analyzer_spec.rb
+++ spec/lib/numeron/analyzer_spec.rb
@@ -4,22 +4,32 @@ require 'spec_helper'
 
 describe Numeron::Analyzer do
   let(:analyzer) { Numeron::Analyzer.new(calc) }
-  describe '0e1b' do
+  describe '#run_average_mode' do
+    context '0e1b' do
+      let(:calc) { Numeron::Calculator.new.tap{|f| f.input('123', 0, 1) } }
+      it do
+        result = analyzer.run_average_mode
+        result[:recommend].should have(378).items
+        result[:size].should == 60.0
+      end
+    end
+  end
+
+  describe '#run_worstcase_mode' do
     let(:calc) { Numeron::Calculator.new.tap{|f| f.input('123', 0, 1) } }
     it do
-      result = analyzer.run
-      result[:size].tapp
-      result[:recommend].first.tapp
+      result = analyzer.run_worstcase_mode
+      result[:recommend].should have(21).items
+      result[:size].should == 72
     end
-    # it do
-    #   result = analyzer.calc_expected_value('567')
-    #   p result
-    # end
-    # it do
-    #   result = analyzer.calc_expected_value('210')
-    #   p result
-    # end
-    # it do
-    # end
+  end
+
+  describe '#run_possibilities' do
+    let(:calc) { Numeron::Calculator.new.tap{|f| f.input('123', 0, 3) } }
+    it do
+      result = analyzer.run_possibilities
+      result[:recommend].should =~ %w(312 231)
+      result[:size].should == 0
+    end
   end
 end
